6afb0e8032c1e513c822cea7108f0d89e3fa7ad1
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-02-16 23:44:30 -0700

    grabbing random 6 letter word from txt file

--- wordle_clone.sh
+++ wordle_clone.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+six_letter_words=6letters.txt
 echo "Weclome to this Wordle Clone"
 read -p "Guess a five letter word to start: " guessedWord
 
@@ -9,3 +10,6 @@ while [[ $length -lt 5 ]] || [[ $length -gt 5 ]]; do
     read -p "Incorrect length, try again: " guessedWord
     length=${#guessedWord}
 done
+
+randomWord=$(shuf -n 1 $six_letter_words)
+echo $randomWord
