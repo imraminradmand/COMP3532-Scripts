12fed9d8cb79b53da9936162b081e8bf79871f6c
Ramin <ramin.radmand@gmail.com>
2022-03-07 14:14:16 -0700

    variable name fixes

--- checkHash/checkHash.sh
+++ checkHash/checkHash.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-hashSignature=$(cat ./filehashoutput.txt)
+providedHashValue=$(cat ./filehashoutput.txt)
 
 for file in *{1..4}.txt; do
     hashedFile=$(sha512sum $file | grep -o '^\S*')
 
-    if [[ $hashedFile == $hashSignature ]]; then
+    if [[ $hashedFile == $providedHashValue ]]; then
 	echo "${file} matches the hash signature"
+	exit 0
     fi
 done
 
-exit 0
+echo "None of the files match the provided hash value"
+
+exit 1>&2
